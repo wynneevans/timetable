9e5989ee42eb360c6fddab754bc95b04c8d1a346
Wynne Evans <wynne@protonmail.com>
2020-04-03 18:05:36 +0100

    Copy files from old room-scheduler repository.

---
+++ room_scheduler/website/views.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponse
+from meetings.models import Meeting
+
+def welcome(request):
+    return render(request, "website/welcome.html",
+                  {"meetings": Meeting.objects.all()})
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("My name is Wynne")
